69b33b0e9c25b2e039bfab76f4334b01168c8c16
Bartosz Bugajski <bugajpcmr@gmail.com>
2023-07-08 15:38:54 +0200

    added pages Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Sign_in from "./pages/Sign_in";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <div>
       <>
-        <h1>React App</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/sign-in" element={<Sign_in />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </Routes>
+        </Router>
       </>
     </div>
   );
---
+++ src/pages/Sign_in.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Sign_in() {
+  return (
+    <div>Sign in</div>
+  )
+}
+
